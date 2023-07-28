350ce757d5726dbce496411cc849556f00908130
Francwagura <kt297francis@gmail.com>
2023-07-28 20:57:19 +0300

    few changes

--- webwinesApi/settings.py
+++ webwinesApi/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'webwinesApi.wsgi.application'
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:5173',
     'http://localhost:3000',
-    'a37f-105-162-10-120.ngrok-free.app',
+    'https://a37f-105-162-10-120.ngrok-free.app',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
